c12d13c2183b6c73213878febce2ecefb0aaa182
zuliandri2 <zuliyandri@gmail.com>
2019-09-20 00:21:43 +0700

    update -index.js -index.html -style.css

--- js/index.js
+++ js/index.js
@@ -2,11 +2,14 @@ const  X = "X";
 const  O = "O";
 
 let table_row = 3,
-    table_column = 3;
+    table_column = 3,
+    p1Color = '#000',
+    p2Color = '#000';
 
 function load() {
     let html = '', i = 0, j = null;
 
+    $('table').empty();
     while (i < table_row) {
         j = 0;
         html += '<tr>';
@@ -24,10 +27,19 @@ function load() {
 
 var assign = new function() {
     let content = null,
-        increament = 0;
+        increament = 0,
+        flagsWin = false;
 
     this.getContent = function ()  {
         return content;
+    };
+
+    this.getFlagsWin = function () {
+        return flagsWin;
+    };
+
+    this.setFlagsWin = function (params) {
+        flagsWin = params;
     };
 
     this.setContent = function (param) {
@@ -172,34 +184,54 @@ function assigntxo(e) {
         column = e.target.dataset.column,
         cb = new checkingBuilder();
 
-    if (d.toString().toUpperCase() != X && d.toString().toUpperCase() != O) {
-        if (assign.getContent() == X) {
-            assign.setContent(O);
-            storeXO.setTabung(row, column, assign.getContent());
-            e.target.innerText = assign.getContent();
-            if ( cb.check(storeXO.getTabungO(), row, column, assign.getContent()) ) {
-                alert('Player 2 win');
-                return;
+    if (!assign.getFlagsWin()) {
+        if (d.toString().toUpperCase() != X && d.toString().toUpperCase() != O) {
+            if (assign.getContent() == X) {
+                assign.setContent(O);
+                storeXO.setTabung(row, column, assign.getContent());
+                $(e.target)
+                    .html(assign.getContent())
+                    .css('color', p2Color);;
+                if (cb.check(storeXO.getTabungO(), row, column, assign.getContent())) {
+                    assign.setFlagsWin(true);
+                    alert('Player 2 win');
+                }
+            } else {
+                assign.setContent(X);
+                storeXO.setTabung(row, column, assign.getContent());
+                $(e.target)
+                    .html(assign.getContent())
+                    .css('color', p1Color);
+                if (cb.check(storeXO.getTabungX(), row, column, assign.getContent())) {
+                    assign.setFlagsWin(true);
+                    alert('Player 1 win');
+                }
             }
-        } else {
-            assign.setContent(X);
-            storeXO.setTabung(row, column, assign.getContent());
-            e.target.innerText = assign.getContent();
-            if (cb.check(storeXO.getTabungX(), row, column, assign.getContent()) ) {
-                alert('Player 1 win');
-                return;
-            }
-        }
 
-        assign.count();
-        if (assign.getCount() == (table_row * table_column)) {
-            alert('Tie');
+            assign.count();
+            if (assign.getCount() == (table_row * table_column) && assign.getFlagsWin() ==  false) {
+                alert('Tie');
+            }
         }
     }
 }
 
-function addRowColumn() {
+function sliderRowColumn() {
+     var v = $('#row_column');
+     $('#show_slider').html(v.val());
+}
 
+function apply() {
+    let row_column = $('#row_column').val(),
+        border_colour= $('#border_colour').val(),
+        player1_clour = $('#p1Color').val(),
+        player2_clour = $('#p2Colour').val();
+
+    table_column = row_column;
+    table_row = row_column;
+    p1Color = player1_clour;
+    p2Color = player2_clour;
+    load();
 }
 
 function reload() {
@@ -208,8 +240,9 @@ function reload() {
 
 $(document).ready(function () {
     load();
-    $('td').on('click', assigntxo);
-    $('#addRowColumn', addRowColumn);
+    $('#show_slider').html($('#row_column').val());
+    $('table').on('click', 'td', assigntxo);
     $('#reset').on('click', reload);
-
+    $('#apply').on('click', apply);
+    $('#row_column').change(sliderRowColumn);
 });
